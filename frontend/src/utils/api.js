548955ae86be7b5669ade0bf1fb94e776b8bf0ae
Aqib <aqibarif0611@gmail.com>
2026-07-14 00:37:54 +0530

    Use environment variable for backend API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,17 @@
-//centeralized API setup
+// Centralized API setup
 
-import axios from 'axios';
-import qs from 'qs';
+import axios from "axios";
+import qs from "qs";
 
 const api = axios.create({
-    baseURL: '/api',
-    withCredentials: true,
-    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
-})
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:9090/api",
+  withCredentials: true,
+  paramsSerializer: (params) =>
+    qs.stringify(params, {
+      arrayFormat: "repeat",
+    }),
+});
 
 export default api;
